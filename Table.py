ac832d53802a0154378ca101b56f650ca8ac3a1d
Ali Hasan <alihasancsestudent@gmail.com>
2026-03-02 03:59:22 +0000

    Do practice

---
+++ Table.py
@@ -0,0 +1,6 @@
+def table(number):
+    for i in range(1,11):
+        print(f"{number} X {i} = {number*i}")
+
+if __name__ == "__main__":
+    print(__name__)
---
+++ teat_table.py
@@ -0,0 +1,6 @@
+from Table import table
+
+if __name__ == "__main__":
+    table(2)
+
+print(__name__)
