855d5436dc096137d37c0aa0418c21372d8e1e07
Ali Hasan <alihasancsestudent@gmail.com>
2026-03-04 15:04:28 +0000

    Remove Exists() method with try catch

--- file-Handling/Read_a_file.py
+++ file-Handling/Read_a_file.py
@@ -1,10 +1,6 @@
-import os
 
-# Open the file in read mode
-file_path = 'file-Handling/sample.txt'
-
-if os.path.exists(file_path):
-    with open(file_path, 'r') as file:
+try:
+    with open('file-Handling/sample.txt', 'r') as file:
         # Read the entire content of the file
         content = file.readlines()
         print("Read file Content:")
@@ -12,5 +8,5 @@ if os.path.exists(file_path):
         for line in content:
             line_number += 1
             print(f"Line {line_number}: {line.strip()}")
-else:
-    print(f"The file '{file_path}' does not exist.")
+except FileNotFoundError:
+    print(f"The file 'sample.txt' does not exist.")
